68f7039f7df00641e8cacc930c35e9cbd8386f61
Marina Voropai <marina.voropai@gmail.com>
2023-01-15 18:18:55 +0600

    Added prefixes to columns, cfg replaced by toml

--- api_weather.py
+++ api_weather.py
@@ -5,12 +5,14 @@ import logging
 import gzip
 import requests
 import shutil
+import tomli
+
 import os
 
 load_dotenv()
 
 def get_weather_forecast(city_name, api_key):
-    '''Получает данные из API '''
+    '''Получает данные из API и возвращает список с содержимым ответа'''
     weather = []
     url = "http://api.openweathermap.org/data/2.5/weather"
     for city in city_name:
@@ -22,23 +24,35 @@ def get_weather_forecast(city_name, api_key):
         }
         
         try:
-            res = requests.get(url, params=params, timeout=1)
+            res = requests.get(url, params=params, timeout=2)
         except requests.exceptions.Timeout:
             logging.error("timeout raised, recovering")
 
         data = res.json()
         weather_city = {}
+
         for key, value in data.items():
             if isinstance(value, dict):
+                # Допишу к каждому ключу нижнего уровня ключ верхнего уровня 
+                source = data[key]
+                result = {}
+
+                for k, v in source.items():
+                    new_key = key + '_' + k
+                    result.update({
+                        new_key : v
+                    })
+
                 weather_city.update(data[key])
             elif isinstance(value, list):
                 value = value[0]
+                value["weather_id"] = value.pop("id")
                 weather_city.update(value)
             else:
                 weather_city.update({key:value})
         # Название города есть в name - поэтому его добавлять не стала
         weather_city.update({
-            'measuring_dt': datetime.now().timestamp()
+            'request_st': datetime.now().timestamp()
         })
         weather.append(weather_city)
     return weather
@@ -52,8 +66,9 @@ def convert_list_to_gzip(lst):
             shutil.copyfileobj(f_in, f_out)
 
 
-with open("./city_name.cfg", "r") as file:
-    city_names = file.read().split(',')
+with open("./city_name.toml", "rb") as file:
+    city_names_in_file = tomli.load(file)
+    city_names = city_names_in_file['city_names']
 
 if __name__=='__main__':
     api_key = os.getenv('my_api_key')
